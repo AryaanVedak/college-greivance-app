1272a47fa0f079203be6b78e0160a5baa0b18079
Broadam24 <aryaanvedak@gmail.com>
2023-01-24 16:52:17 +0530

    login page design

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -10,6 +11,127 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            // Login text
+            Text(
+              "Login",
+              style: GoogleFonts.poppins(
+                  fontWeight: FontWeight.w700, fontSize: 36),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Welcome to Top G app",
+              style: GoogleFonts.poppins(fontSize: 24, color: Colors.black54),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+
+            // Login text field
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(12)),
+                child: const Padding(
+                  padding: EdgeInsets.only(left: 10.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.email,
+                          color: Color.fromARGB(255, 152, 71, 233)),
+                      border: InputBorder.none,
+                      hintText: 'Email',
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            const SizedBox(
+              height: 20,
+            ),
+
+            // Password text field
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(12)),
+                child: const Padding(
+                  padding: EdgeInsets.only(left: 10.0),
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.password,
+                          color: Color.fromARGB(255, 152, 71, 233)),
+                      border: InputBorder.none,
+                      hintText: 'Password',
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            const SizedBox(
+              height: 40,
+            ),
+
+            // Sign in button
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Container(
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                    color: Colors.deepPurple,
+                    borderRadius: BorderRadius.circular(12)),
+                child: Center(
+                  child: Center(
+                      child: Text(
+                    'Sign in',
+                    style: GoogleFonts.poppins(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 20),
+                  )),
+                ),
+              ),
+            ),
+
+            const SizedBox(
+              height: 25,
+            ),
+
+            // Don't have an account? Register Now
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              // ignore: prefer_const_literals_to_create_immutables
+              children: [
+                Text(
+                  "Don't have an account?",
+                  style: GoogleFonts.poppins(fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  " Register Now",
+                  style: GoogleFonts.poppins(
+                      color: Colors.blue, fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
